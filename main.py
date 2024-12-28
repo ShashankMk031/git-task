4f11e8d8e5029ee56f4eb0da336f341ed6415367
Shashank <shashankmk031@gmail.com>
2024-12-28 14:37:55 +0530

    corrected the error

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 Bunch of functions
 """
 
-def add(number_a:int, number_b:int):
+def add(number_a: int, number_b: int) -> int:
     """
-    This function adds 2 numbers
+    This function adds 2 numbers.
     """
-    return number_a  + number_b + 1
+    return number_a + number_b
+
     
